86991b5743ba6c1a18cd8616f5eb8fdc725e3ebc
surachat12345 <51730656+surachat12345@users.noreply.github.com>
2019-07-10 10:08:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,19 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+   # sendText(user,userText
+    if (userText == 'สวัสดี') :
+       sendText(user('เออ! ดี')
+    elif (userText == 'ไป ล่ะ') :
+        sendText(user('เออ! บาย')
+    else :
+        sendText(user('ไม่เข้าใจว่ะ')
+   sendText(user('เออ! ดี')
+    elif (userText == 'ไป ล่ะ') :
+       sendText(user('เออ! บาย')
+    else :
+        sendText(user('ไม่เข้าใจว่ะ')
+
     return '',200
 
 def sendText(user, text):
